ec61a2f5e6a9764c3c3d9e83fa1baeb53d2a5bab
josemip98 <j_miguel98@hotmail.com>
2020-11-22 16:07:41 +0100

    Añado funciones

--- api/fecha.ts
+++
@@ -1,6 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node'
-
-export default (request: NowRequest, response: NowResponse) => {
-    let { hoy = new Date().toLocaleString() } = request.query
-    response.status(200).send(`La fecha actual es: ${hoy}`)
-}
